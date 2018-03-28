2d9aa60c23c1e6bc3bba6df9341638e55dbb35d8
captain <980097191@qq.com>
2018-03-28 14:41:21 +0800

    upload

---
+++ ChangePngSizeForTexturePackerToMinisize/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace MultiSizeIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(66, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 109);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "缩小比较大的图片,一般是界面底图，让图集打出来是合适的大小";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "MultiSizeIcon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ChangePngSizeForTexturePackerToMinisize/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace MultiSizeIcon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //首先读取配置文件
+            if(File.Exists("./ChangePngSizeForTexturePackerToMinisize.txt")==false)
+            {
+                MessageBox.Show("Not Found ChangePngSizeForTexturePackerToMinisize.txt");
+                return;
+            }
+
+            StreamReader tmpStreamReaderConfig = new StreamReader("./ChangePngSizeForTexturePackerToMinisize.txt");
+            List<string> tmpListImageDataStr = new List<string>();
+            while (tmpStreamReaderConfig.EndOfStream==false)
+            {
+                tmpListImageDataStr.Add(tmpStreamReaderConfig.ReadLine());
+            }
+
+
+            List<string> tmpListPngFileJump = new List<string>();
+            for (int i = 0; i < tmpListImageDataStr.Count; i++)
+            {
+                string tmpImageDataStr = tmpListImageDataStr[i];
+                tmpImageDataStr = tmpImageDataStr.Trim();
+                if (tmpImageDataStr.StartsWith("#"))
+                {
+                    continue;
+                }
+                string[] tmpImageDataStrArr = tmpImageDataStr.Split(',');
+                if(tmpImageDataStrArr.Length!=4)
+                {
+                    continue;
+                }
+
+                string tmpImageName = tmpImageDataStrArr[0];
+                string tmpImageWidthStr = tmpImageDataStrArr[1];
+                string tmpImageHeightStr = tmpImageDataStrArr[2];
+                string tmpImageScaleStr = tmpImageDataStrArr[3];
+                int tmpImageWidth = int.Parse(tmpImageWidthStr);
+                int tmpImageHeight = int.Parse(tmpImageHeightStr);
+                float tmpImageScale = float.Parse(tmpImageScaleStr);
+
+                //处理图片
+                FileStream stream = File.OpenRead("./"+ tmpImageName);
+                int fileLength = 0;
+                fileLength = (int)stream.Length;
+                Byte[] image = new Byte[fileLength];
+                stream.Read(image, 0, fileLength);
+                System.Drawing.Image result = System.Drawing.Image.FromStream(stream);
+                stream.Close();
+
+                //判断图片是否已经处理过
+                if(result.Width!=tmpImageWidth || result.Height!=tmpImageHeight)
+                {
+                    tmpListPngFileJump.Add(tmpImageName);
+                    continue;
+                }
+
+
+                Size s = new Size((int)(tmpImageScale*result.Width), (int)(tmpImageScale*result.Height));
+                Bitmap newBit = new Bitmap(result, s);
+
+                File.Move("./" + tmpImageName, "./" + tmpImageName + ".bak");
+
+                newBit.Save("./" + tmpImageName);
+
+                File.Delete("./" + tmpImageName + ".bak");
+
+                newBit.Dispose();
+            }
+
+
+            string tmpMsg = string.Empty;
+            if(tmpListPngFileJump.Count>0)
+            {
+                tmpMsg += "处理完毕，跳过的文件有" + tmpListPngFileJump.Count + "个,";
+                for (int i = 0; i < tmpListPngFileJump.Count; i++)
+                {
+                    tmpMsg += Environment.NewLine+tmpListPngFileJump[i];
+                }
+            }
+            else
+            {
+                tmpMsg += "处理完毕，跳过的文件有0个,";
+            }
+            MessageBox.Show(tmpMsg);
+        }
+    }
+}
